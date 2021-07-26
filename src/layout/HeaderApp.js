c41eb2c3bc352403dfedda1b8b7d022e8545c8db
Gabriel9Stresser <gabriel.augusto99@hotmail.com>
2021-07-26 13:20:14 -0300

    last items

--- src/layout/HeaderApp.js
+++ src/layout/HeaderApp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../assets/images/logo.svg'
+import logo from './../assets/images/logo.svg'
 import {Link} from "react-router-dom";
 
 const HeaderApp = () => {
